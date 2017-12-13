bcf29fb2941666bb83664e279501386d4e047e7e
Patrick 1 <Patrick@Patricks-MacBook-Pro.local>
2017-12-13 14:05:30 -0800

    completed setup, no challenges

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 // SERVER-SIDE JAVASCRIPT
 
-
+var db = require('./models')
 /////////////////////////////
 //  SETUP and CONFIGURATION
 /////////////////////////////
@@ -25,29 +25,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //  DATA
 ///////////////////
 
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
+// server.js
+app.get('/api/books', function (req, res) {
+  // send all books as JSON response
+  db.Book.find(function(err, books){
+    if (err) {
+      console.log("index error: " + err);
+      res.sendStatus(500);
+    }
+    res.json(books);
+  });
+});
 
 
 var newBookUUID = 18;
